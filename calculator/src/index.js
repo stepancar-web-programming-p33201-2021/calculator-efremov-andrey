0a9e33a6cb73f4639b1a2d28860ef5a9717913b2
Yasumitai <andreyefr2001@mail.ru>
2021-12-14 19:58:05 +0300

    .gitignore updated devDependencies added selectors deprecated

--- calculator/src/index.js
+++ calculator/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 
 const buttons = ['+', '1', '2', '3', 'br', '-', '4', '5', '6', 'br', '*', '7', '8', '9', 'br', '%', 'x^y', '0', '=', 'br', 'clear']
 
-class MarkdownEditor extends React.Component {
+class Calculator extends React.Component {
     constructor(props) {
         super(props);
         this.handleChange = this.handleChange.bind(this);
@@ -14,20 +14,18 @@ class MarkdownEditor extends React.Component {
 
     handleChange(e) {
         this.setState({value: e.target.value});
+        console.log(this.state.value)
     }
 
     handleClick(e) {
-        let current = document.getElementById("input-area").value
+        let current = this.state.value
         if (e.target.id === 'clear') {
-            document.getElementById("input-area").value = ''
             this.setState({value: ''})
         } else if (e.target.id === '=')
-            document.getElementById("input-area").value = this.calc()
+            this.calc()
         else if (e.target.id === 'x^y') {
-            document.getElementById("input-area").value = current + '^'
             this.setState({value: current + '^'})
         } else {
-            document.getElementById("input-area").value = current + e.target.id
             this.setState({value: current + e.target.id})
         }
     }
@@ -36,30 +34,23 @@ class MarkdownEditor extends React.Component {
         let nums = this.state.value.toString()
         let result
         if (nums.match(/[^0123456789+\-*%^ ]/)) {
-            this.setState({value: ''})
-            return "Incorrect input"
-        }
-        else if (nums.includes("+")) {
+            result = "Incorrect input"
+        } else if (nums.includes("+")) {
             nums = nums.split("+")
             result = parseInt(nums[0]) + parseInt(nums[1])
-        }
-        else if (nums.includes("-")) {
+        } else if (nums.includes("-")) {
             nums = nums.split("-")
             result = parseInt(nums[0]) - parseInt(nums[1])
-        }
-        else if (nums.includes("*")) {
+        } else if (nums.includes("*")) {
             nums = nums.split("*")
             result = parseInt(nums[0]) * parseInt(nums[1])
-        }
-        else if (nums.includes("%")) {
+        } else if (nums.includes("%")) {
             nums = nums.split("%")
             result = Math.floor(parseInt(nums[0]) / parseInt(nums[1]))
-        }
-        else if (nums.includes("^")) {
+        } else if (nums.includes("^")) {
             nums = nums.split("^")
             result = Math.pow(parseInt(nums[0]), parseInt(nums[1]))
-        }
-        else
+        } else
             result = nums
         this.setState({value: result})
         return result
@@ -71,13 +62,13 @@ class MarkdownEditor extends React.Component {
                 <textarea
                     id="input-area"
                     onChange={this.handleChange}
-                    defaultValue={this.state.value}
+                    value={this.state.value}
                 />
                 <br/>
                 {buttons.map(item => {
                     if (item === 'br')
                         return (<br/>)
-                    else{
+                    else {
                         return (<button id={item} onClick={this.handleClick}>{item}</button>)
                     }
                 })}
@@ -87,6 +78,6 @@ class MarkdownEditor extends React.Component {
 }
 
 ReactDOM.render(
-    <MarkdownEditor/>,
+    <Calculator/>,
     document.getElementById('root')
 );
